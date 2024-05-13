d0f9d10b9f03a22b02865c641402f45e3dc8edc4
Subhabrata-Maity <subhobroto2211@gmail.com>
2024-05-14 00:43:10 +0530

    removing files for fresh project

--- Models/UserConnection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChatAppUsingSignalR.Models
-{
-    public class UserConnection
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string ChatRoom { get; set; } = string.Empty;
-    }
-}
